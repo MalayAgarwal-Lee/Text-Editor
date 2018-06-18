7e5488a32e488ac8792619dcf829795c169babb3
Malay Agarwal <malay.agarwal261016@outlook.com>
2018-06-18 14:56:19 +0530

    Add special characters for formatting buttons

---
+++ unicode_chars.py
@@ -0,0 +1,17 @@
+import platform
+
+operating_system = platform.system()
+
+if operating_system == 'Windows':
+
+    bold = '\uE19B'
+    italic = '\uE199'
+    underline = '\uE19A'
+
+elif operating_system == 'Linux':
+
+    bold = ''
+    italic = ''
+    underline = ''
+
+strike = 'S̵t̵r̵i̵k̵e̵'
