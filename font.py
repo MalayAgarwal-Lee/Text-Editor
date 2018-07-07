a89a960571da896455292e21fbe00cb8705a4591
Malay Agarwal <malay.agarwal261016@outlook.com>
2018-07-07 19:03:53 +0530

    Small font change for Ubuntu

--- font.py
+++ font.py
@@ -6,7 +6,7 @@ OP_SYS = platform.system()
 
 def get_font():
     if OP_SYS == 'Linux':
-        return ('Ubuntu Mono', 12)
+        return ('Ubuntu', 12)
     elif OP_SYS == 'Windows':
         return ('Segoe UI', 12)
     else:
